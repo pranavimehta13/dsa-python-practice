7497cc8263bf579d080509e29e1d9e6576fa97bb
Pranavi Mehta <pranavimehta13@gmail.com>
2026-02-25 00:18:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.py
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.py
@@ -1,12 +1,7 @@
 class Solution:
     def isSorted(self, arr) -> bool:
-        flag = 0
         n = len(arr)
-        for i in range(n-1):
-            if arr[i] > arr[i+1]:
-                flag = 1
-                break
-        if flag == 0:
-            return True
-        else:
-            return False
+        for i in range(1,n):
+            if arr[i]<arr[i-1]:
+                return False
+        return True
