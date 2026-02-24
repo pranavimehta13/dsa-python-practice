5e30fc8c38b813144bab027c280d45a9492d21f6
Pranavi Mehta <pranavimehta13@gmail.com>
2026-02-25 00:09:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -1,8 +1,12 @@
 class Solution:
     def getSecondLargest(self, arr):
-        largest = max(arr)
+        largest = arr[0]
         second_largest = -1
         for i in range(len(arr)):
-            if arr[i] > second_largest and arr[i] != largest:
+            if arr[i] > largest:
+                largest = arr[i]
+        for i in range(len(arr)):
+            if arr[i] > second_largest and arr[i]!=largest:
                 second_largest = arr[i]
         return second_largest
+            
