20520a889555b0e32c3c95f510a10295608b2523
Pranavi Mehta <pranavimehta13@gmail.com>
2026-02-26 12:47:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Addition of two square matrices/addition-of-two-square-matrices.py
@@ -0,0 +1,10 @@
+# User function Template for python3
+class Solution:
+    def Addition(self, matrixA, matrixB):
+        n = len(matrixA)
+
+        for i in range(n):
+            for j in range(n):
+                matrixA[i][j] = matrixA[i][j] + matrixB[i][j]
+
+        return matrixA
