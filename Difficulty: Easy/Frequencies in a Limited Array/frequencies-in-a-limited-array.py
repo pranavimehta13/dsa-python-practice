8260481dd677d02391965a995d8a33a316505e29
Pranavi Mehta <pranavimehta13@gmail.com>
2026-02-27 10:10:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.py
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def frequencyCount(self, arr):
-        n = len(arr)
-        hash_list = [0]*(n+1)
-        for num in arr:
-            hash_list[num]+=1
-        return hash_list[1:]
-
+       n = len(arr)
+       hash_list = [0]*(n+1)
+       for num in arr:
+           hash_list[num] += 1
+       return hash_list[1:]
+       
