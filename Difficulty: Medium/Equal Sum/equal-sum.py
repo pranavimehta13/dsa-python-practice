4e9f87aa1377d776b7c760818950f20648ff20ab
Pranavi Mehta <pranavimehta13@gmail.com>
2026-03-14 11:33:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equal Sum/equal-sum.py
+++ Difficulty: Medium/Equal Sum/equal-sum.py
@@ -2,11 +2,9 @@ class Solution:
     def equilibrium(self, arr):
         total = sum(arr)
         left_sum = 0
-
         for num in arr:
             total -= num
-            if left_sum == total:
+            if total == left_sum:
                 return "true"
             left_sum += num
-
         return "false"
